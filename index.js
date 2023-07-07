9c3479e4faff8ee30d6bfc100dd8038852910df3
Terrence <terrence.liu@alumni.ubc.ca>
2023-07-06 23:50:28 -0700

    add all implementation in index/iss

---
+++ index.js
@@ -0,0 +1,17 @@
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = (data) => {
+  for (const obj of data) {
+    let date = new Date(obj.risetime * 1000).toString();
+    const duration = obj.duration;
+    console.log(`Next pass at ${date} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, data) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  
+  printPassTimes(data);
+});
